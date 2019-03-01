dd4ee2ddc41f60a89c3d08de0e23e52c36cfee63
Alex <dinotrooper@gmail.com>
2019-03-01 12:14:04 -0600

    init commit

---
+++ logParser.py
@@ -0,0 +1,7 @@
+#top level part of the program that calls the other parts of the program which are above
+def top_level():
+    
+
+#just calls top level part of programs
+if __name__ = '__main__':
+    top_level()
